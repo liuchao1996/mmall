bb89f2985680067338d504d4c7454872908eb766
1659419264@qq.com <147258gitee>
2019-08-05 00:00:33 +0800

    Initial commit

---
+++ src/main/java/com/MmallApplication.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: mmall
+ * @description:
+ * @author: lc
+ * @create: 2019-08-04 22:44
+ * @version: 1.0
+ **/
+@SpringBootApplication
+public class MmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmallApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/alipay/demo/trade/Test.java
@@ -0,0 +1,18 @@
+package com.alipay.demo.trade;
+
+import java.util.UUID;
+
+/**
+ * @program: mmall
+ * @description: TEST
+ * @author: lc
+ * @create: 2019-08-04 09:33
+ * @version: 1.0
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID().toString().replaceAll("-", ""));
+    }
+
+}
